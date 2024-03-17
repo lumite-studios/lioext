53b8f976ed560e3f1091428d29e64bd990fe25f3
crotanite <crotanite@lumitestudios.com>
2024-03-17 19:24:43 +0000

    feat: add new news post

--- resources/data/news.ts
+++ resources/data/news.ts
@@ -1,5 +1,9 @@
 export const posts: Array<{ date: string, content: string }> = [
     {
+        date: '2024-03-17',
+        content: 'Added option to include nursing cubs to LioImage.'
+    },
+    {
         date: '2024-02-17',
         content: 'Complete overhaul of LioExt. Removed "LioGene" and "LioMart" for now, may look at adding them back later. Image generation has been reworked, and I hope its a little bit faster now too.'
     },
